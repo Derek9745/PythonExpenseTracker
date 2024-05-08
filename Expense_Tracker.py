61aba5266fcef3af1bdd8f69c68414562e1f4bea
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2024-05-08 14:08:10 -0400

    Fixed ID errors, adjusted expense tracker class, button error handling

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -1,88 +1,162 @@
+
 from tkinter import *
 from tkinter import ttk
-import tkinter
+import tkinter as tk
 from tkinter import messagebox
 import openpyxl
 from datetime import datetime
+from tkinter import scrolledtext 
+import sqlite3
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 
 class Expense:
-    def __init__(self, date,description, amount, action, TransID):
+    def __init__(self, date,description, amount, note, TransID):
         self.date = date
         self.description = description
         self.amount = amount
-        self.action = action
+        self.note = note
         self.TransID = TransID
       
 class ExpenseTracker:
     current_ID_Counter = 0
     def __init__(self):
         self.expenses = []
-        
+             
     def transaction_id(self):
         ExpenseTracker.current_ID_Counter += 1
         return ExpenseTracker.current_ID_Counter
 
     def add_expense(self):
-        action = "----------------"
-        TransID = self.transaction_id()
-
-        while True:
-            try:
-                date = date_entry.get()
-                datetime.strptime(date, '%m/%d/%Y')
-                break
-            except ValueError:
-                retry = messagebox.askretrycancel(title="Input Error", message = "Invalid input. Please enter a valid date input")
-                if not retry:
-                    return  
-                 
-        description = name_entry.get()
-
-        while True:
-            try:
-                amount = float(amount_entry.get())
-                break
-            except ValueError:
-                retry = messagebox.askretrycancel(title="Input Error", message = "Invalid input. Please enter a valid numerical input")
-                if not retry:
-                    return 
-
-        expense = Expense(TransID, date, description, amount, action)
-        self.expenses.append(expense)
-        treeview.insert("", tkinter.END, values = (expense.date,expense.description,expense.amount,expense.action,expense.TransID))
-
+        if len(name_entry.get()) != 0 and len(date_entry.get()) != 0 and len(amount_entry.get()) != 0:
+                try:
+                    description = name_entry.get()
+                    amount = float(amount_entry.get())
+                    date = datetime.strptime(date_entry.get(),'%m/%d/%Y')
+                    note = "----------------"
+                    TransID = self.transaction_id()
+                    expense = Expense(TransID, date, description, amount, note)
+                    self.expenses.append(expense)
+                    treeview.insert("", tk.END, values = (expense.date,expense.description,expense.amount,expense.note,expense.TransID))
+                except ValueError:
+                    messagebox.askretrycancel(title="Input Error", message = "Invalid input. Please enter a valid input")
+        else:
+            messagebox.askretrycancel(title="Input Error", message = "Invalid input. All input fields are empty. No row added")
 
     def remove_expense(self):
         selected_item = treeview.selection() 
-        if selected_item:  
+        values = treeview.item(selected_item, "values")
+        if values:
+            transID = values[0]
+            for expense in self.expenses:
+                if expense.TransID == transID:
+                    self.expenses.remove(expense)
+                    break  # Stop looping once the expense is found and removed
             treeview.delete(selected_item)
+       
+
         else:
             messagebox.askretrycancel(title="Input Error", message = "No Row Selected")
-       
+            
         
     def total_expenses(self):
         total = sum(expense.amount for expense in self.expenses)
         return total
 
     
-    def generate_plot(self):
-        self.total_expenses()
-        ## create a horizontal bar chart that compares expenses to spending budget
+    def generate_plot(self, budget):
+        total_expenses = self.total_expenses()
+
+        # Data for plotting
+        categories = ["Expenses", "Budget"]
+        values = [total_expenses, budget]
+
+        # Create bar plot
+        fig, ax = plt.subplots()
+        ax.barh(categories, values, color=['blue', 'green'])
+        ax.set_xlabel('Amount')
+        ax.set_title('Expenses vs Spending Budget')
+
+        # Embed the plot into the GUI
+        canvas = FigureCanvasTkAgg(fig, master=plot_label)
+        canvas.draw()
+        #canvas.get_tk_widget().configure(width=250, height=125)
+        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+
+
+
+
+
 
     def add_note(self):
         selected_item = treeview.selection() 
         if selected_item:  
-                treeview.set(selected_item, column="Action", value="Note")
+            if note_entry.get("1.0", "end-1c") != "":
+                treeview.set(selected_item, column="Note", value="note")
+                note = note_entry.get("1.0", tk.END)
+                conn = sqlite3.connect("notes.db")
+                cursor = conn.cursor()
+                cursor.execute("CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)")
+                cursor.execute("INSERT INTO notes (content) VALUES (?)", (note,))
+                conn.commit()
+                conn.close()
+            else:
+                messagebox.askretrycancel(title="Input Error", message = "No Text Entered")
         else:
             messagebox.askretrycancel(title="Input Error", message = "No Row Selected")
-        
+
+
+    def view_note(self):
+        selected_item = treeview.selection()
+        if selected_item:
+            values = treeview.item(selected_item, "values")
+            note = values[4]
+            if note != "----------------":
+                conn = sqlite3.connect("notes.db")
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM notes")
+                notes = cursor.fetchall()
+                conn.close()
+
+                view_window = tk.Toplevel(Window)
+                view_window.title("View Notes")
+                view_text = tk.Text(view_window)
+                for note in notes:
+                    view_text.insert(tk.END, note[1] + "\n")
+                view_text.pack()
+            else:
+                messagebox.askretrycancel(title="Input Error", message = "No note created for this expense")
+            #bring up text from note into textview
+
+        else:
+            messagebox.askretrycancel(title="Input Error", message = "No Row Selected")
+
+    def delete_note(self):
+        selected_item = treeview.selection()
+        if selected_item:
+            values =  treeview.item(selected_item, "values")
+            note = values[4]
+            if note == "note":
+                treeview.set(selected_item, column="Note", value="----------------")
+                conn = sqlite3.connect("notes.db")
+                cursor = conn.cursor()
+                cursor.execute("DELETE FROM notes")
+                conn.commit()
+                conn.close()
+            else: 
+                messagebox.askretrycancel(title = "Input Error", message = "No note created for this expense")
+        else:
+            messagebox.askretrycancel(title="Input Error", message = "No Row Selected")
+
+
     def load_excel(self):
         path = "C:/Users/hehnd/OneDrive/Desktop/Python/PythonExpenseTracker/Expense_Data.xlsx"
         workbook = openpyxl.load_workbook(path)
         sheet = workbook.active
         list_values = list(sheet.values)
-        action = "----------------"
+        note = "----------------"
         print(list_values)
         for col_name in list_values[0]:
             col_name = str(col_name)
@@ -93,9 +167,9 @@ class ExpenseTracker:
             date_str =value_tuple[0].strftime('%m/%d/%Y')
             description = value_tuple[1]
             amount = value_tuple[2]
-            expense = Expense(TransID, date_str, description, amount, action)
+            expense = Expense(TransID, date_str, description, amount, note)
             self.expenses.append(expense) 
-            treeview.insert('', tkinter.END, values=(expense.date,expense.description,expense.amount, expense.action,expense.TransID))
+            treeview.insert('', tk.END, values=(expense.date,expense.description,expense.amount, expense.note,expense.TransID))
       
 if __name__ == "__main__":
 
@@ -107,34 +181,52 @@ if __name__ == "__main__":
     style.theme_use("forest-dark")
     Window.minsize(1000,600)
     Window.title("Expense Tracker")
+    
 
     frame = ttk.Frame(Window)
     frame.pack()
 
-    widgets_frame = ttk.LabelFrame(frame,text="Insert Row")
+    command_frame = ttk.LabelFrame(frame,text = "Commands")
+    command_frame.grid(row = 0, column = 0,padx = 20, pady = 10)
+
+    widgets_frame = ttk.LabelFrame(command_frame,text="Insert Row")
     widgets_frame.grid(row=0,column = 0, padx = 20, pady = 10)
 
-    widgets_frame_2 = ttk.LabelFrame(frame,text="Delete Row")
-    widgets_frame_2.grid(row=2,column = 0, padx = 20, pady = 5)
+    widgets_frame_2 = ttk.LabelFrame(command_frame,text="Delete Row")
+    widgets_frame_2.grid(row=1,column = 0, padx = 20, pady = 5)
 
-    widgets_frame_3 = ttk.LabelFrame(frame,text="View Spending Report")
-    widgets_frame_3.grid(row=3,column = 0, padx = 20, pady = 5)
+    widgets_frame_3 = ttk.LabelFrame(command_frame,text="View Spending Report")
+    widgets_frame_3.grid(row=2,column = 0, padx = 20, pady = 5)
 
-    widgets_frame_4 = ttk.LabelFrame(frame,text="Load Excel DataSheet")
-    widgets_frame_4.grid(row=4,column = 0, padx = 20, pady = 5)
+    outer_frame = ttk.LabelFrame(frame, text = "DashBoard")
+    outer_frame.grid(row = 2, column = 0, padx = 5 , pady = 2, columnspan = 3)
 
-    widgets_frame_5 = ttk.LabelFrame(frame, text = "Spending Report Vizualization")
-    widgets_frame_5.grid(row = 2, column = 1, padx = 5 , pady = 5)
+    widgets_frame_4 = ttk.LabelFrame(command_frame,text="Load Excel DataSheet")
+    widgets_frame_4.grid(row=3,column = 0, padx = 20, pady = 5)
 
-    widgets_frame_6 = ttk.LabelFrame(frame, text = "Add Note")
-    widgets_frame_6.grid(row = 5, column = 0, padx = 5 , pady = 5)
+    widgets_frame_5 = ttk.LabelFrame(outer_frame, text = "Spending Report Vizualization")
+    widgets_frame_5.grid(row = 0, column = 1, padx = 5 , pady = 5)
 
-    widgets_frame_7 = ttk.LabelFrame(frame, text = "View Note")
-    widgets_frame_7.grid(row = 6, column = 0, padx = 5 , pady = 5)
+    widgets_frame_6 = ttk.LabelFrame(command_frame, text = "Add Note")
+    widgets_frame_6.grid(row = 4, column = 0, padx = 5 , pady = 5)
+
+    widgets_frame_7 = ttk.LabelFrame(command_frame, text = "View Note")
+    widgets_frame_7.grid(row = 5, column = 0, padx = 5 , pady = 5)
+
+    widgets_frame_8 = ttk.LabelFrame(command_frame, text = "Delete Note")
+    widgets_frame_8.grid(row = 6, column = 0, padx = 5 , pady = 5)
 
 
     plot_label = ttk.Label(widgets_frame_5, text="Plot Label")
-    plot_label.grid(row=0, column=1, columnspan=1)
+    plot_label.grid(row=0, column=1)
+
+    note_text_entry = ttk.LabelFrame(outer_frame, text = "Note Text Entry")
+    note_text_entry.grid(row = 0, column = 0, padx = 5 , pady = 2)
+
+
+    note_entry = scrolledtext.ScrolledText(note_text_entry, wrap=tk.WORD, width=110, height=13, font=("Calibri", 12))
+    note_entry.grid(row =0, column =0, padx = 5,pady = 5)
+
 
     name_entry = ttk.Entry(widgets_frame)
     name_entry.insert(0,"Name")
@@ -152,6 +244,7 @@ if __name__ == "__main__":
     amount_entry.grid(row = 2,column = 0,padx = 5, pady = 5, sticky="ew")
 
     tracker = ExpenseTracker()
+   
 
     insert_button = ttk.Button(widgets_frame, text = "Insert", command=tracker.add_expense)
     insert_button.grid(row=3, column = 0, padx = 5, pady = 5,sticky = "nsew")
@@ -159,7 +252,7 @@ if __name__ == "__main__":
     delete_button = ttk.Button(widgets_frame_2, text = "Delete Selected Row", command = tracker.remove_expense)
     delete_button.grid(row=0, column = 0, padx = 5, pady = 5,sticky = "nsew")
 
-    view_data_button = ttk.Button(widgets_frame_3, text = "View Spending Report", command = tracker.generate_plot)
+    view_data_button = ttk.Button(widgets_frame_3, text = "View Spending Report", command = lambda: tracker.generate_plot(1500))
     view_data_button.grid(row=0, column = 0, padx = 5, pady = 5,sticky = "nsew")
 
     load_data_button = ttk.Button(widgets_frame_4, text = "Load Excel DataSheet", command = tracker.load_excel)
@@ -168,14 +261,18 @@ if __name__ == "__main__":
     add_note = ttk.Button(widgets_frame_6, text = "Add Note", command = tracker.add_note)
     add_note.grid(row=0, column = 0, padx = 5, pady = 5,sticky = "nsew")
 
-    view_note = ttk.Button(widgets_frame_7, text = "View Note")
+    view_note = ttk.Button(widgets_frame_7, text = "View Note", command = tracker.view_note)
     view_note.grid(row=0, column = 0, padx = 5, pady = 5,sticky = "nsew")
 
+    delete_note = ttk.Button(widgets_frame_8, text = "Delete Note", command = tracker.delete_note)
+    delete_note.grid(row=0, column = 0, padx = 5, pady = 5,sticky = "nsew")
+
     treeFrame = ttk.Frame(frame)
-    treeFrame.grid(row=0,column = 1, pady = 10)
+    treeFrame.grid(row=0,column = 1, pady = 0)
     treeScroll = ttk.Scrollbar(treeFrame)
     treeScroll.pack(side="right", fill = "y")
-    treeview = ttk.Treeview(treeFrame,column=("Transaction ID","Date","Description","Amount","Action"),show= "headings",height= 8)
+    treeview = ttk.Treeview(treeFrame,column=("Transaction ID","Date","Description","Amount","Note"),show= "headings",height= 29)
+    treeview.pack()
 
 
     treeview.column("Transaction ID",anchor=CENTER)
@@ -186,10 +283,10 @@ if __name__ == "__main__":
     treeview.heading("Description", text= "Description")
     treeview.column("Amount",anchor=CENTER)
     treeview.heading("Amount", text= "Amount")
-    treeview.column("Action",anchor=CENTER)
-    treeview.heading("Action", text= "Action")
+    treeview.column("Note",anchor=CENTER)
+    treeview.heading("Note", text= "Note")
 
 
-    treeview.pack()
+    
     treeScroll.config(command = treeview.yview)
     Window.mainloop()
